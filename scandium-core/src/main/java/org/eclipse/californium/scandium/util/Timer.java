463d21f4fa7eff9b26e512cc80bfb839d46952e4
caocan <wwwcaocan@sina.cn>
2018-04-02 11:03:11 +0800

    在Scandium上进行功能搭建，有bug，目前的bug是ClientHello中的identity_list不能正确传递给服务端

---
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/util/Timer.java
@@ -0,0 +1,10 @@
+package org.eclipse.californium.scandium.util;
+
+//用于描述时间
+public class Timer {
+    public static int no;
+    public static long start;
+    public static long end;
+    public static String KeyExchange;
+    public static int MaxLoop = 1;
+}
